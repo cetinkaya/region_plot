daaebed2c3a4953519aeeeb1f2f4027c1dbdabf6
Ahmet Cetinkaya <cetinkayaahmet@yahoo.com>
2019-08-26 14:23:14 +0900

    Add examples.

---
+++ examples/example0.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import matplotlib.animation as animation
+import region_plot as rp
+
+def example0():
+    def pred(x, y):
+        return np.sin(x*x) + 3*y*y <= 1
+    pl.figure(figsize=(5, 5))
+    rp.region_plot(pred, (-2, 2), (-2, 2))
+    pl.xlabel("$x$", fontsize=14)
+    pl.ylabel("$y$", fontsize=14)
+    pl.title("$\sin(x^2)+3y^2 \leq 1$", fontsize=14)
+    pl.tight_layout()
+    pl.savefig("example0.png")
+
+example0()
---
+++ examples/example1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import matplotlib.animation as animation
+import region_plot as rp
+
+def example1():
+    def pred(x, y):
+        return np.sin(x*x) + 3*y*y <= 1
+    pl.figure(figsize=(5, 5))
+    rp.region_plot(pred, (-2, 2), (-2, 2), discovery=0.01, nof_expansions=5000, facecolor="#9090FF", alpha=0.5)
+    pl.xlabel("$x$", fontsize=14)
+    pl.ylabel("$y$", fontsize=14)
+    pl.title("$\sin(x^2)+3y^2 \leq 1$", fontsize=14)
+    pl.tight_layout()
+    pl.savefig("example1.png")
+
+example1()
